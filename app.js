585b7744ec4356d3e5ad8dd7e6695c60f61ce772
gurukun <70239069+gurukun@users.noreply.github.com>
2020-09-11 15:24:17 -0700

    Update app.js
    Small changes

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-//const json ='{"Dinos":[{"species":"Triceratops","weight":13000,"height":114,"diet":"herbavor","where":"North America","when":"Late Cretaceous","fact":"First discovered in 1889 by Othniel Charles Marsh"},{"species":"Tyrannosaurus Rex","weight":11905,"height":144,"diet":"carnivor","where":"North America","when":"Late Cretaceous","fact":"The largest known skull measures in at 5 feet long."},{"species":"Anklyosaurus","weight":10500,"height":55,"diet":"herbavor","where":"North America","when":"Late Cretaceous","fact":"Anklyosaurus survived for approximately 135 million years."},{"species":"Brachiosaurus","weight":70000,"height":"372","diet":"herbavor","where":"North America","when":"Late Jurasic","fact":"An asteroid was named 9954 Brachiosaurus in 1991."},{"species":"Stegosaurus","weight":11600,"height":79,"diet":"herbavor","where":"North America, Europe, Asia","when":"Late Jurasic to Early Cretaceous","fact":"The Stegosaurus had between 17 and 22 seperate places and flat spines."},{"species":"Elasmosaurus","weight":16000,"height":59,"diet":"carnivor","where":"North America","when":"Late Cretaceous","fact":"Elasmosaurus was a marine reptile first discovered in Kansas."},{"species":"Pteranodon","weight":44,"height":20,"diet":"carnivor","where":"North America","when":"Late Cretaceous","fact":"Actually a flying reptile, the Pteranodon is not a dinosaur."},{"species":"Pigeon","weight":0.5,"height":9,"diet":"herbavor","where":"World Wide","when":"Holocene","fact":"All birds are living dinosaurs.<br/><br/>"}]}'
-//let objectJson = JSON.parse(json)
-
 //store data
 let allCreatures = []
 
@@ -42,11 +39,12 @@ Creature.prototype.heightCompare = function(human){
 Creature.prototype.weightCompare = function(human){
   let weightFact = "";
   if (human > this.weight) {
-    weightFact = "You are heavier than " + this.species + ". That means you are heavier than dinosors!";
+    const difference = human - this.weight;
+    weightFact = `You are ${difference} lb heavier than ${this.species}!`;
   } else if (human === this.weight) {
     weightFact = "You and " + this.species + " are equally tall.";
   } else {
-    weightFact = "You are lighter than " + this.species + ". Yes, dinosors are heavy!";
+    weightFact = "You are lighter than " + this.species + ". Yes, dinosaurs are heavy!";
   }
   this.fact.push(weightFact)
 };
